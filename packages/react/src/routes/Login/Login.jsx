bd3a1ee7d0c29f18b352b77c3ebf8dd8f21d2102
Prabashwara Seneviratne <contact@bash.lk>
2024-03-29 12:47:17 +0100

    added ability to register

--- packages/react/src/routes/Login/Login.jsx
+++ packages/react/src/routes/Login/Login.jsx
@@ -1,11 +1,13 @@
 import { useState, useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { login } from '@timer-app/common/api';
+import { login, register } from '@timer-app/common/api';
 import { UserContext } from '@timer-app/common/context/UserContextProvider';
 import Container from '@timer-app/common/components/Container';
 import Input from '@timer-app/common/components/Input';
 import Button from '@timer-app/common/components/Button';
 import Title from '@timer-app/common/components/Title';
+import { ButtonVariants } from '@timer-app/common/components/Button/Button';
+import StatusMessage from '@timer-app/common/components/StatusMessage';
 import styles from './Login.module.css';
 
 const Login = ({ history }) => {
@@ -20,15 +22,28 @@ const Login = ({ history }) => {
 
     const handleFormSubmit = (e) => {
         e.preventDefault();
+        const action = e.nativeEvent.submitter.value;
         const formData = new FormData(e.target);
         const username = formData.get('username');
         const password = formData.get('password');
-        login(username, password).then((response) => {
-            user.setAuthenticatedUser(response);
-            history.replace('/');
-        }).catch(() => {
-            setLoginError('Invalid username or password');
-        });
+
+        if (action == 'login') {
+            login(username, password).then((response) => {
+                user.setAuthenticatedUser(response);
+                history.replace('/');
+            }).catch(() => {
+                setLoginError('Invalid username or password');
+            });
+        }
+
+        if (action == 'register') {
+            register(username, password).then((response) => {
+                user.setAuthenticatedUser(response);
+                history.replace('/');
+            }).catch(() => {
+                setLoginError('Username already exists');
+            });
+        }
     };
 
     return (
@@ -37,8 +52,11 @@ const Login = ({ history }) => {
             <form className={styles['login__form']} action="" onSubmit={handleFormSubmit}>
                 <Input name="username" type="text" placeholder="Username" required />
                 <Input name="password" type="password" placeholder="Password" required />
-                <Button type="submit">Login</Button>
-                {loginError && <p>{loginError}</p>}
+                {loginError && <StatusMessage className={styles['login__status']} message={loginError} />}
+                <div className={styles['login__button']}>
+                    <Button value="login" type="submit">Login</Button>
+                    <Button variant={ButtonVariants.SECONDARY} value="register" type="submit">Register</Button>
+                </div>
             </form>
         </Container>
     );
