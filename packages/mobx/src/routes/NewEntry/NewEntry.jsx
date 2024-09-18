2585720a01c4d83d1307aa69c2b07035e5cb3405
Prabashwara Seneviratne <contact@bash.lk>
2024-09-18 19:22:16 +0200

    copied tanstack-query to create mobx

---
+++ packages/mobx/src/routes/NewEntry/NewEntry.jsx
@@ -0,0 +1,109 @@
+import { useEffect, useRef, useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import { createEntry } from '@timo/common/api';
+import Timer from '@timo/common/components/Timer';
+import Title from '@timo/common/components/Title';
+import Input from '@timo/common/components/Input';
+import Button, { ButtonVariants } from '@timo/common/components/Button';
+import StatusMessage from '@timo/common/components/StatusMessage';
+import styles from './NewEntry.module.css';
+
+const TimerState = {
+    ACTIVE: 'active',
+    PAUSED: 'paused',
+    STOPPED: 'stopped'
+};
+
+const NewEntry = () => {
+    const descriptionRef = useRef(null);
+    const [duration, setDuration] = useState(0);
+    const [timerState, setTimerState] = useState(TimerState.STOPPED);
+
+    const { mutate: create, error, isPending, isSuccess } = useMutation({
+        mutationFn: createEntry
+    });
+    const statusMessage =
+        error ? error.message :
+            isPending ? 'Saving...' :
+                isSuccess ? 'Time logged successfully' : '';
+
+    const handleStartClick = (e) => {
+        e.preventDefault();
+        setTimerState(TimerState.ACTIVE);
+    };
+
+    const handleStopClick = () => {
+        setTimerState(TimerState.STOPPED);
+    };
+
+    const handlePauseClick = () => {
+        setTimerState(TimerState.PAUSED);
+    };
+
+    useEffect(() => {
+        if (timerState === TimerState.STOPPED && duration > 0) {
+            setDuration(0);
+            const endTimestamp = Math.floor(Date.now() / 1000);
+            create({
+                description: descriptionRef.current.value,
+                start_time: `@${endTimestamp - duration}`,
+                end_time: `@${endTimestamp}`
+            });
+        }
+    });
+
+    return (
+        <form action="" onSubmit={handleStartClick}>
+            <Title>New time entry</Title>
+            <div className={styles['center']}>
+                <Timer
+                    value={duration}
+                    active={timerState === TimerState.ACTIVE}
+                    onPaused={setDuration}
+                />
+                <Input
+                    name="description"
+                    ref={descriptionRef}
+                    className={styles['description']}
+                    label="Time entry description"
+                    type="text"
+                    placeholder="What are you working on?"
+                    required
+                />
+            </div>
+            <StatusMessage
+                message={statusMessage}
+                className={styles['status']}
+            />
+            <div className={styles['buttons']}>
+                {(timerState === TimerState.STOPPED && duration === 0) && (
+                    <Button type="submit">
+                        Start
+                    </Button>
+                )}
+                {(timerState === TimerState.PAUSED && duration !== 0) && (
+                    <>
+                        <Button onClick={handleStartClick}>
+                            Resume
+                        </Button>
+                        <Button variant={ButtonVariants.SECONDARY} onClick={handleStopClick}>
+                            Finish
+                        </Button>
+                    </>
+                )}
+                {timerState === TimerState.ACTIVE && (
+                    <>
+                        <Button onClick={handlePauseClick}>
+                            Pause
+                        </Button>
+                        <Button variant={ButtonVariants.SECONDARY} onClick={handleStopClick}>
+                            Stop
+                        </Button>
+                    </>
+                )}
+            </div>
+        </form>
+    );
+};
+
+export default NewEntry;
---
+++ packages/mobx/src/routes/Profile/Profile.jsx
@@ -0,0 +1,37 @@
+import { useMutation } from '@tanstack/react-query';
+import Title from '@timo/common/components/Title';
+import Button, { ButtonVariants } from '@timo/common/components/Button';
+import useUser from '@timo/common/hooks/useUser';
+import { logout } from '@timo/common/api';
+import styles from './Profile.module.css';
+import ChangePassword from './sections/ChangePassword';
+import CustomizeUser from './sections/CustomizeUser';
+
+const Profile = () => {
+    const user = useUser();
+
+    const { mutate: logoutM } = useMutation({
+        mutationFn: logout,
+        onSuccess: () => {
+            user.clearUser();
+        }
+    });
+
+    return (
+        <div className={styles['profile']}>
+            <Title>My profile</Title>
+            <CustomizeUser />
+            <ChangePassword />
+            <Button
+                className={styles['sign-out']}
+                value="login"
+                variant={ButtonVariants.SECONDARY}
+                onClick={logoutM}
+            >
+                Sign out
+            </Button>
+        </div>
+    );
+};
+
+export default Profile;
