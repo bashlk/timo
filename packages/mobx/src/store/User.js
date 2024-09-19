edf2f0c0d3c0424873474a2568d6e41690636ebc
Prabashwara Seneviratne <contact@bash.lk>
2024-09-19 12:28:42 +0200

    fixed topbar re-rendering for unrelated changes

--- packages/mobx/src/store/User.js
+++ packages/mobx/src/store/User.js
@@ -5,7 +5,7 @@ import { UserStatus } from '@timo/common/context/UserContextProvider';
 export class User {
     status = UserStatus.UNKNOWN;
     // has to be a normal property and not private for makeAutoObservable to make it observable
-    data = null;
+    data = {};
     error = null;
 
     constructor() {
@@ -16,7 +16,10 @@ export class User {
     // MobX in React strict mode requires all mutations to happen within actions
     setUser = (user) => {
         this.status = UserStatus.AUTHENTICATED;
-        this.data = user;
+        this.data.id = user.id;
+        this.data.username = user.username;
+        this.data.avatar_character = user.avatar_character;
+        this.data.avatar_background = user.avatar_background;
         this.error = null;
     };
 
