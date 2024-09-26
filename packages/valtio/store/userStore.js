4c59557c1d8eec8040b2a9dcb17ba44c1ca6b9de
Prabashwara Seneviratne <contact@bash.lk>
2024-09-26 17:21:14 +0200

    stopped reassigned data in user proxy

--- packages/valtio/store/userStore.js
+++ packages/valtio/store/userStore.js
@@ -4,32 +4,44 @@ import { getUser } from '@timo/common/api';
 
 const userStore = proxy({
     status: UserStatus.UNKNOWN,
-    data: {},
+    data: {
+        id: null,
+        username: null,
+        avatar_character: null,
+        avatar_background: null
+    },
     error: null
 });
 
+export const setUser = (user) => {
+    userStore.status = UserStatus.AUTHENTICATED;
+    userStore.data = user;
+
+    userStore.data.id = user.id;
+    userStore.data.username = user.username;
+    userStore.data.avatar_character = user.avatar_character;
+    userStore.data.avatar_background = user.avatar_background;
+};
+
 export const fetchUser = () => {
     getUser()
-        .then((user) => {
-            userStore.status = UserStatus.AUTHENTICATED;
-            userStore.data = user;
-            userStore.error = null;
-        })
+        .then(setUser)
         .catch((error) => {
             userStore.status = UserStatus.UNAUTHENTICATED;
             userStore.error = error;
         });
 };
 
-export const setUser = (user) => {
-    userStore.status = UserStatus.AUTHENTICATED;
-    userStore.data = user;
-};
-
 export const clearUser = () => {
     userStore.status = UserStatus.UNAUTHENTICATED;
-    userStore.data = null;
     userStore.error = null;
+
+    userStore.data = {
+        id: null,
+        username: null,
+        avatar_character: null,
+        avatar_background: null
+    };
 };
 
 // Fetch the user during app startup
